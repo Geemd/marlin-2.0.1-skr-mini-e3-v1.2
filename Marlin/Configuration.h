f92cf54324032b98ac68db5a282cd50038b3a90e
Unknown <gregor@geemd.de>
2020-01-11 18:58:03 +0100

    Change PID autotune bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -527,9 +527,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 117.29
+  #define DEFAULT_bedKi 22.86
+  #define DEFAULT_bedKd 401.18
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
